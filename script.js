083fe18b7738fca9ffb21e6362ec6a9d3365501a
Eve <evestroud89@gmail.com>
2022-04-05 14:46:04 -0400

    Fix script tag and add computerPlay function

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ const output = document.querySelector('.output')
 //
 // function computerPlay
 // 	randomly chooses from RPS
+function computerPlay() {
+	choice = Math.floor(Math.random() * 3);
+	if (choice === 0) {
+		return 'rock';
+	} else if (choice === 1) {
+		return 'paper';
+	} else {
+		return 'scissors';
+	}
+}
 
 // function playTurn(playerSelection, computerSelection)
 // 	play a single round of RPS
