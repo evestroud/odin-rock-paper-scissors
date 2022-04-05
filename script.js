d2e1179be7fa2ddeba6ac70b222a9f3255b5dc75
Eve <evestroud89@gmail.com>
2022-04-04 22:24:52 -0400

    Initialize HTML, CSS, and JS files

---
+++ script.js
@@ -0,0 +1,13 @@
+// function computerPlay
+// 	randomly chooses from RPS
+
+// function playTurn(playerSelection, computerSelection)
+// 	play a single round of RPS
+// 	return a string that declares the winner
+// 	make sure input is case insensitive
+
+// function Game
+// 	calls playRound for a 5 round game
+//	use prompt() to get input from user
+
+// make more helper functions if necessary
